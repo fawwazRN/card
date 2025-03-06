ea653f2f3a05755d5472375c26f2a881038d56f4
fawwazRN <fawwaz1511@student.abudzar.sch.id>
2025-03-06 09:30:22 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+let tombol = document.getElementsByClassName('button');
+if (tombol = true){
+    alert('Produk berhasil ditambahkan ke keranjang!');
+} else {
+    alert('masukkan ke keranjang')
+}
